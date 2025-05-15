3db9c162d7997ca97d9572f0648c50fe9aa45355
Sara Carlstein <carlstein.sara@gmail.com>
2025-05-15 16:29:43 +0200

    Add custom favicon and update head tags for Swedish website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="sv">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
